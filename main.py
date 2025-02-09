a98b641afb513d72ad2af105018f3c091936c935
rtree <arakitomohiko@gmail.com>
2025-02-09 09:46:17 +0900

    removed unessesary import

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import tweepy
-from dotenv import load_dotenv
-from openai import OpenAI
-import requests
-from bs4 import BeautifulSoup
 from collections import deque
 import asyncio
-from datetime import datetime
-from PyPDF2 import PdfReader
-from io import BytesIO
-import base64
 import sys
 
 from config import Config
