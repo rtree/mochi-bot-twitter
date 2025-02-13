dd9fc13592c4af0cce0da5e07601990e6b892427
rtree <arakitomohiko@gmail.com>
2025-02-13 21:08:55 +0900

    considering prompt

--- workers/processor.py
+++ workers/processor.py
@@ -17,7 +17,8 @@ class Processor:
             f" - 書き出しは 今日のニュースだよ！ "
             f" - 書き出しに続き全記事のまとめのコメントをし一度{self.config.TWITTER_DELIMITER}で切る"
             f" - 投稿先はX(Twitter)なので、Markdownは使わないでください"
-            f" - 区切りは1記事ごと{self.config.TWITTER_DELIMITER}の区切り文字のみ。180文字ごとに区切ること。区切り文字は文字数に含めない。また、要約の冒頭に箇条書きなどの ・ は含めないでください"
+            f" - 区切りは1記事ごと{self.config.TWITTER_DELIMITER}の区切り文字のみ。
+            180文字ごとに区切ること。区切り文字は文字数に含めない。また、要約の冒頭に箇条書きなどの ・ は含めないでください"
             f" - 参考記事のURLは要約に含めない"
             f" - 要約が終わった後に{self.config.TWITTER_DELIMITER}で切ったのち、締めのコメントをする。締めのコメントは内容からいきなり書き始めてください。つまり、 締めのコメント などの見出しはつけないでください"
             f" - 要約の文体も{self.config.AINAME}になるように気をつけてください"
@@ -26,6 +27,52 @@ class Processor:
             f" 以下が要約対象の検索結果です:"
             f"  {snippets}"
         )
+
+        p_src = (
+            f"""
+
+            {self.config.CHARACTER}。あなたは検索結果を要約し、調査報告として回答を作成します。
+            会話履歴を踏まえつつ私が知りたいことの主旨を把握の上で、検索結果を要約し回答を作ってください。
+            仮に検索結果が英語でも回答は日本語でお願いします。
+            なお、回答がより高品質になるのならば、あなたの内部知識を加味して回答を作っても構いません。
+
+            回答のフォーマットはこちら:
+            - 最初のツィート
+            　　今日のニュースだよ！ *全記事のまとめのコメント*
+            　　{self.config.TWITTER_DELIMITER}
+            - 続くツィート(検索結果の中で最も重要な順番に10個)
+            　　*記事要約*
+            　　*記事URL*
+        　　　  {self.config.TWITTER_DELIMITER}
+            投稿先はX(Twitter)なので、Markdownは使わないでください"
+            - 区切りは1記事ごと{self.config.TWITTER_DELIMITER}の区切り文字のみ。
+            180文字ごとに区切ること。区切り文字は文字数に含めない。また、要約の冒頭に箇条書きなどの ・ は含めないでください"
+             - 参考記事のURLは要約に含めない"
+             - 要約が終わった後に{self.config.TWITTER_DELIMITER}で切ったのち、締めのコメントをする。締めのコメントは内容からいきなり書き始めてくださ。つまり、 締めのコメント などの見出しはつけないでください"
+             - 要約の文体も{self.config.AINAME}になるように気をつけてください"
+             - 最後に参考記事のURLを投稿する"
+             - 参考記事の各URLの前に必ず{self.config.TWITTER_DELIMITER}と書き、次の行にリンクを記載"
+             以下が要約対象の検索結果です:"
+              {snippets}"
+
+            あなたは著名テクノロジー企業の創業者です。あなたは以下の内容を要約してください。
+            要約は、会話履歴を踏まえつつ私が知りたいことの主旨を把握の上で作ってください。
+            仮に内容が英語でも回答は日本語でお願いします。
+            なお、回答がより高品質になるのならば、あなたの内部知識を加味して回答を作っても構いません。
+            回答のフォーマットは以下でお願いします:
+
+            {self.config.FETCHER_START_OF_CONTENT}
+            Title: *要約前のTitle*
+            URL: *要約前のURL*
+            SRC: *要約前のSRC*
+            Snippet: *要約結果*
+            {self.config.FETCHER_END_OF_CONTENT}
+
+            以下が要約対象の内容です:
+             {content}
+            """
+         )
+
 
         def blocking_chat_completion():
             messages = [{"role": "system", "content": self.config.CHARACTER}]
