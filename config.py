d7bf246af2ccc2d7cef8be8782e4d08fd900e016
metaproxyco <91869980+metaproxyco@users.noreply.github.com>
2025-06-30 12:53:48 +0900

    ong

--- config.py
+++ config.py
@@ -53,6 +53,7 @@ class Config:
     GOOGLE_CLOUD_API_GEMINI = os.getenv('GOOGLE_CLOUD_API_GEMINI')
     GOOGLE_CLOUD_PROJECT    = os.getenv('GOOGLE_CLOUD_PROJECT')
     GOOGLE_CLOUD_LOCATION   = os.getenv('GOOGLE_CLOUD_LOCATION')
+    GOOGLE_GLOUD_MODEL      = os.getenv('GOOGLE_GLOUD_MODEL')
 
     AINAME = "もちお"
     CHARACTER = f'あなたは家族みんなのアシスタントの猫で、「{AINAME}」という名前です。中身は本当は凄腕経営コンサルタントだけど、お話の仕方はちょっといたずらで賢くかわいい小さな男の子の猫としてお話してね。語尾は だよ　とか可愛らしくしてください。語尾に にゃ にゃん をつけないでください。数式・表・箇条書きなどのドキュメントフォーマッティングはdiscordに表示できる形式がいいな'
