29e66a2a14a97b845c0e3c9c02472ae336936900
rtree <arakitomohiko@gmail.com>
2025-07-02 18:55:44 +0200

    Update grokFetcher.py

--- workers/grokFetcher.py
+++ workers/grokFetcher.py
@@ -34,7 +34,7 @@ class GrokFetcher:
          )
 
         response = self.client.chat.completions.create(
-            model=config.GROK_MODEL,
+            model=self.config.GROK_MODEL,
             messages=[
                 {"role": "system", "content": "You are a social media analyst."},
                 {"role": "user", "content": p_src},
